4ba9c48159250d3c67fd4fdf28323f3c01b6043e
SofiyaShaikh24 <132351797+SofiyaShaikh24@users.noreply.github.com>
2023-05-17 18:45:45 +0530

    Day7

---
+++ Interface/Interface/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace Interface
+{
+    
+    interface ICustomer
+    {
+        void Iface1();
+    }
+
+    interface ICustomer2
+    {
+        void Iface2();
+    }
+
+    public class cust : ICustomer2
+    {
+        public void Iface1()
+        {
+            Console.WriteLine("Interface 1");
+        }
+
+        public void Iface2()
+        {
+            Console.WriteLine("Interface 2");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            cust customer = new cust();
+            customer.Iface1();
+            customer.Iface2();
+        }
+    }
+}
